30d41de8be5b2c70661779235f4c871e32b2f101
Bruno Roberto Gomes <bruno@brgomes.com>
2022-06-08 10:29:11 -0300

    Módulo 4 ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import Vue from 'vue'
 import VueToastify from 'vue-toastify'
 
+Vue.config.devtools = true
 Vue.use(VueToastify)
 
 Vue.component('posts-component', require('./components/Posts/Posts.vue').default)
